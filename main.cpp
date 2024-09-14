30604a607af5f2458a4d28244e93ae53ab82fe27
briphi2384 <111828397+briphi2384@users.noreply.github.com>
2024-09-14 11:15:03 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Module 1
+//
+//  Created by Brian Phipps on 9/14/24.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    
+    std::cout << "First Name: John \n";
+    cout << "Last Name: Doe" <<endl;
+    std::cout << "Address: 123 Colorado Ave\n";
+    std::cout << "City: Denver \n";
+    std::cout << "ZIP: 12345 \n";
+    
+    return 0;
+}
