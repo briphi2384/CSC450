492111f52b34dcd8f872101ac161a7cb73a8cd01
briphi2384 <111828397+briphi2384@users.noreply.github.com>
2024-10-23 17:52:34 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,56 +1,45 @@
 //  main.cpp
-//  CT_Mod_5
-//  Created by Brian Phipps on 10/9/24.
+//  CTAmod7
+//  Created by Brian Phipps on 10/23/24.
+
 #include <iostream>
-#include <fstream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx;
+std::condition_variable var;
+bool isDone = false; //will indicate when first thread finishes
+
+//first thread counts up to 20
+void countUp(){
+    for (int i =0; i <=20; i++){ // repeat 20 times
+        std::lock_guard<std::mutex> lock(mtx); //locks mutex
+        std::cout<<"Count: "<<i << std::endl;
+    }//end for
+    //set flag and notify second thread
+    std::lock_guard<std::mutex> lock(mtx); //lock mutex
+    isDone = true;
+    var.notify_one(); //notifies second thread to start counting down
+}
+
+//second thread counts down to 0
+void countDown() {
+    std::unique_lock<std::mutex> lock(mtx); //locks mutex for synchronization
+    var.wait(lock, [] {return isDone;}); //waits for first thread to finish
+    
+    //when notified, begin counting down
+    for (int i =20; i >=0; --i){
+        std::cout <<"Count: " << i << std::endl;
+    }
+}
 
 int main() {
+    //create and join the two threads
+    std::thread t1(countUp);
+    std::thread t2(countDown);
     
-    std::string originalFile = "/Users/briansmacbook/Downloads/CSC450_CT5_mod5.txt"; //file to be appended. Change file path as needed
-    std::string reverseFile = "/Users/briansmacbook/Desktop/School/CSU Global/CSC450/CSC450-mod5-reverse.txt"; //new file for reversal, update filepath as needed
-    
-    std::ofstream outfile;
-    outfile.open(originalFile, std::ios::app); //open and append file
-    
-    if (!outfile.is_open()){ //verify file was opened
-        std::cout<<"Error opening file\n";
-    }else{
-        std::cout<<"Success! File was opened\n";
-    }
-    
-    std::string userInput; //retrieve user input
-    std::cout<<"Enter text to append: \n";
-    std::getline(std::cin, userInput);
-    
-    outfile << userInput << std::endl; //append input to og file
-    outfile.close();
-    
-    //begin reversing the file
-    std::ifstream infile(originalFile);
-    if (!infile.is_open()) {
-        std::cout<<"Error opening file\n";
-    }
-    
-    //reads every char from file, adds to string "content"
-    std::string content;
-    char ch;
-    while (infile.get(ch)){
-        content += ch;
-    }
-    infile.close();
-    std::reverse(content.begin(), content.end()); //reverses content string
-    
-    //create and open new reverse file
-    std::ofstream reverseOutfile(reverseFile);
-    if (!reverseOutfile.is_open()){
-        std::cout<< "Error opening file\n";
-    }else{
-        std::cout<<"Success!, new file was opened\n";
-        std::cout<<"Reverse file created at: " << reverseFile <<std::endl;
-    }
-    //write content string to new file,
-    reverseOutfile << content;
-    reverseOutfile.close();
+    t1.join();
+    t2.join();
     
     return 0;
 }
